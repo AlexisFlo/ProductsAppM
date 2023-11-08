478b72740f1d7698df82fa2ca85bcc15589ee6dc
Flowers <floresfernandezalexis05@gmail.com>
2023-11-08 11:24:38 -0600

    Add CardHeader component and styles

---
+++ app/components/card-header/index.jsx
@@ -0,0 +1,19 @@
+import { Ionicons } from "@expo/vector-icons";
+import { View, Text } from "react-native";
+
+import { styles } from "./styles";
+
+const CardHeader = ({ headerTitle, headerIcon, headerIconColor }) => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.headerTitle}>{headerTitle}</Text>
+        <View style={styles.headerAction}>
+          <Ionicons name={headerIcon} size={18} color={headerIconColor} />
+        </View>
+      </View>
+    </View>
+  );
+};
+
+export default CardHeader;
--- app/components/card/index.jsx
+++ app/components/card/index.jsx
@@ -1,11 +1,25 @@
 import { View, Text } from "react-native";
 
 import { styles } from "./styles";
+import CardHeader from "../card-header";
 
-const Card = ({ title, subtitle, primaryColor, secondaryColor }) => {
+const Card = ({
+  title,
+  subtitle,
+  primaryColor,
+  secondaryColor,
+  headerTitle,
+  headerIcon,
+  headerIconColor,
+}) => {
   return (
     <View style={styles.container}>
       <View style={[styles.card, { backgroundColor: primaryColor }]}>
+        <CardHeader
+          headerTitle={headerTitle}
+          headerIcon={headerIcon}
+          headerIconColor={headerIconColor}
+        />
         <Text style={styles.title}>{title}</Text>
         <Text style={styles.subtitle}>{subtitle}</Text>
       </View>
