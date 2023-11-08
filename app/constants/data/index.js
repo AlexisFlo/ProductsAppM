137803a60562967cae624dcd52a3c1cc0f136bf2
Flowers <floresfernandezalexis05@gmail.com>
2023-11-08 13:01:36 -0600

    Update product data with new information

--- app/constants/data/index.js
+++ app/constants/data/index.js
@@ -13,31 +13,33 @@ export const PRODUCTS = [
   {
     id: 2,
     title: "iPad Pro",
-    subtitle: "Liquid Retina XDR display or Liquid Retina displayFootnote, M2 Chip",
+    subtitle: "Liquid Retina XDR display or Liquid Retina displayFootnote², M2 chip",
     price: 799,
-    image:
-      "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/ipad-pro-12-select-cell-spacegray-202104_FMT_WHH?wid=940&hei=1112&fmt=png-alpha&.v=1617130311000",
+    image: "https://mac-center.com.pe/cdn/shop/products/IMG-4500987_823x.png?v=1660920397",
     primaryColor: "#8D48AD",
-    secondaryColor: "#C18DD7",
+    secondaryColor: "#BA8CCF",
+    aspectRatio: 1,
   },
   {
     id: 3,
-    title: 'MacBook Pro 13"',
-    subtitle: "Apple M1 chip, 8-core CPU, 8-core GPU, 8GB unified memory, 256GB SSD storage",
-    price: 1299,
+    title: "Apple Watch Ultra",
+    subtitle:
+      "Always-on Retina display, 100% recycled aluminum casesFootnote³, S7 chip with 64-bit dual-core processor",
+    price: 799,
     image:
-      "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/mbp-spacegray-select-202011_FMT_WHH?wid=904&hei=840&fmt=jpeg&qlt=80&.v=1603406905000",
+      "https://help.apple.com/assets/6398A9D9257A2C1D9026967A/6398A9DD257A2C1D90269684/es_419/079d62243eded1c82a866dcf096ecfac.png",
     primaryColor: "#514F59",
     secondaryColor: "#95939F",
+    aspectRatio: 1,
   },
   {
     id: 4,
-    title: "MacBook Air",
-    subtitle: "Apple M1 chip, 8-core CPU, 7-core GPU, 8GB unified memory, 256GB SSD storage",
-    price: 999,
-    image:
-      "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/macbook-air-space-gray-select-202011?wid=904&hei=840&fmt=jpeg&qlt=80&.v=1603406905000",
+    title: 'MacBook Pro 16"',
+    subtitle: "Apple M2 Pro with 12‑core CPU, 19‑core GPU, 16‑core Neural Engine",
+    price: 2499,
+    image: "https://support.apple.com/library/APPLE/APPLECARE_ALLGEOS/SP858/mbp16-gray.png",
     primaryColor: "#EA484E",
     secondaryColor: "#F4A4A6",
+    aspectRatio: 1,
   },
 ];
