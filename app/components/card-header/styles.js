199d0135bf07cbb8b25ec535a2257c4ae5f096ea
Flowers <floresfernandezalexis05@gmail.com>
2023-11-08 11:42:45 -0600

    Add row layout and header action to card header styles.

--- app/components/card-header/styles.js
+++ app/components/card-header/styles.js
@@ -2,6 +2,22 @@ import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
   container: {
+    flexDirection: "row",
+    padding: 15,
+    marginBottom: 15,
+    alignItems: "center",
+  },
+  header: {
     flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  headerTitle: {
+    fontSize: 14,
+  },
+  headerAction: {
+    flexDirection: "row",
+    justifyContent: "flex-end",
+    alignSelf: "center",
   },
 });
