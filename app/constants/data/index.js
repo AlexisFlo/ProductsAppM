97f5d8ad8ffabff34859c864cac2cc268046f7a8
Flowers <floresfernandezalexis05@gmail.com>
2023-11-08 08:36:52 -0600

    Add new products to PRODUCTS constant

--- app/constants/data/index.js
+++ app/constants/data/index.js
@@ -8,5 +8,32 @@ export const PRODUCTS = [
     primaryColor: '#5156BB',
     secondaryColor: '#888FD0',
     aspectRatio: 1,
+  },
+  {
+    id: 2,
+    title: 'iPad Pro',
+    subtitle: 'Liquid Retina XDR display or Liquid Retina displayFootnote, M2 Chip',
+    price: 799,
+    image: 'https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/ipad-pro-12-select-cell-spacegray-202104_FMT_WHH?wid=940&hei=1112&fmt=png-alpha&.v=1617130311000',
+    primaryColor: '#8D48AD',
+    secondaryColor: '#C18DD7',
+  },
+  {
+    id: 3,
+    title: 'MacBook Pro 13"',
+    subtitle: 'Apple M1 chip, 8-core CPU, 8-core GPU, 8GB unified memory, 256GB SSD storage',
+    price: 1299,
+    image: 'https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/mbp-spacegray-select-202011_FMT_WHH?wid=904&hei=840&fmt=jpeg&qlt=80&.v=1603406905000',
+    primaryColor: '#514F59',
+    secondaryColor: '#95939F',
+  },
+  {
+    id: 4,
+    title: 'MacBook Air',
+    subtitle: 'Apple M1 chip, 8-core CPU, 7-core GPU, 8GB unified memory, 256GB SSD storage',
+    price: 999,
+    image: 'https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/macbook-air-space-gray-select-202011?wid=904&hei=840&fmt=jpeg&qlt=80&.v=1603406905000',
+    primaryColor: '#EA484E',
+    secondaryColor: '#F4A4A6',
   }
 ]
