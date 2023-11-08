162a75ac3eb901fed76f0a0353b677f2e6d58fbc
Flowers <floresfernandezalexis05@gmail.com>
2023-11-08 10:40:42 -0600

    Add primary color to card background and adjust card dimensions.

--- app/components/card/index.jsx
+++ app/components/card/index.jsx
@@ -5,7 +5,7 @@ import styles from "./styles";
 const Card = ({ title, subtitle, primaryColor, secondaryColor }) => {
   return (
     <View style={styles.container}>
-      <View style={styles.card}>
+      <View style={[styles.card, { backgroundColor: primaryColor }]}>
         <Text style={styles.title}>{title}</Text>
         <Text style={styles.subtitle}>{subtitle}</Text>
       </View>
--- app/components/card/styles.js
+++ app/components/card/styles.js
@@ -1,7 +1,30 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet, Dimensions } from "react-native";
 
-export default styles = StyleSheet.create({
+const { width } = Dimensions.get("window");
+
+export const CARD_WIDTH = width * 0.6;
+
+export const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  card: {
+    felx: 1,
+    margin: 30,
+    padding: 15,
+    justifyContent: "space-between",
+    borderRadius: 15,
+  },
+  title: {
+    fontSize: 25,
+    textAlign: "center",
+    color: "#fff",
+    fontWeight: "bold",
+    marginBottom: 15,
+  },
+  subtitle: {
+    fontSize: 14,
+    textAlign: "center",
+    color: "#fff",
   },
 });
