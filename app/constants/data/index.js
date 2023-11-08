13535309207a050ca8545771058adc3ad67970b1
Flowers <floresfernandezalexis05@gmail.com>
2023-11-07 18:36:06 -0600

    Add iPhone 13 Pro to PRODUCTS constant

---
+++ app/constants/data/index.js
@@ -0,0 +1,12 @@
+export const PRODUCTS = [
+  {
+    id: 1,
+    title: 'iPhone 13 Pro',
+    subtitle: 'Super Retina XDR displayfootnote, Dynamic Island, A new may to interact with iPhone',
+    price: 999,
+    image: 'https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone-13-pro-family-hero?wid=940&hei=1112&fmt=png-alpha&.v=1631652055000',
+    primaryColor: '#5156BB',
+    secondaryColor: '#888FD0',
+    aspectRatio: 1,
+  }
+]
