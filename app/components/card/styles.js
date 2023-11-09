fceddc24056d5e0729f6017f09017d3a710bfea0
Flowers <floresfernandezalexis05@gmail.com>
2023-11-09 12:38:47 -0600

    Add shadow and elevation to card component

--- app/components/card/styles.js
+++ app/components/card/styles.js
@@ -13,6 +13,15 @@ export const styles = StyleSheet.create({
     height: CARD_HEIGHT,
     justifyContent: "center",
     alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 1,
+    },
+    shadowOpacity: 0.22,
+    shadowRadius: 2.22,
+
+    elevation: 3,
   },
   card: {
     flex: 1,
