b111fc5c9529df79ac859485ba85910b9d51db06
Flowers <floresfernandezalexis05@gmail.com>
2023-11-09 14:52:33 -0600

    Add ProductItem component and styles.
    Update
    Products screen to render ProductItem component in
    a FlatList

--- app/shop/products.js
+++ app/shop/products.js
@@ -1,24 +1,38 @@
 import { Ionicons } from "@expo/vector-icons";
 import { Tabs, useLocalSearchParams, useRouter } from "expo-router";
 import { View, Text, StyleSheet } from "react-native";
+import { FlatList } from "react-native-gesture-handler";
+import { ProductItem } from "../components";
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    justifyContent: "center",
-    alignItems: "center",
+  },
+  contentContainerList: {
+    paddingTop: 40,
   },
 });
+
+const data = new Array(15).fill(0).map((_, index) => ({ id: index }));
 
 export default function Products() {
   const router = useRouter();
   const params = useLocalSearchParams();
 
+  const renderItem = ({ item }) => {
+    return <ProductItem />;
+  };
+
   console.warn({ params });
   return (
     <View style={styles.container}>
-      <Text>Products</Text>
+      <FlatList
+        data={data}
+        renderItem={renderItem}
+        keyExtractor={(item) => item.id.toString()}
+        contentContainerStyle={styles.contentContainerList}
+      />
     </View>
   );
 }
