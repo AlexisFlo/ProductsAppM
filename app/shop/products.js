c67612016d75fae438a257bac47c5c22c1573108
Flowers <floresfernandezalexis05@gmail.com>
2023-11-09 15:27:58 -0600

    Delete unused ProductItem component and styles, refactor Products component to use a simpler renderItem function.

--- app/shop/products.js
+++ app/shop/products.js
@@ -10,28 +10,29 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#fff",
   },
+  item: {
+    height: 150,
+    width: "90%",
+    backgroundColor: "#B8B6E2",
+    alignSelf: "center",
+    borderRadius: 10,
+    marginTop: 15,
+  },
   contentContainerList: {
     paddingTop: 40,
   },
 });
 
 const data = new Array(15).fill(0).map((_, index) => ({ id: index }));
-
 export default function Products() {
   const router = useRouter();
   const params = useLocalSearchParams();
 
-  const renderItem = ({ item }) => <ProductItem />;
+  const renderItem = ({ item }) => <View style={styles.item} />;
 
-  console.warn({ params });
   return (
     <View style={styles.container}>
-      <FlatList
-        data={data}
-        renderItem={renderItem}
-        keyExtractor={(item) => item.id.toString()}
-        contentContainerStyle={styles.contentContainerList}
-      />
+      <FlatList data={data} renderItem={renderItem} keyExtractor={(item) => item.id.toString()} />
     </View>
   );
 }
