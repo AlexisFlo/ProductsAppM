8b2004a92ad0b43f076e0f4b55846a4c08617a83
Flowers <floresfernandezalexis05@gmail.com>
2023-11-13 11:47:16 -0600

    Add ProductItem component to components directory and use it in Products screen

---
+++ app/components/product-item/styles.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  item: {
+    height: 150,
+    width: "90%",
+    backgroundColor: "#B8B6E2",
+    alignSelf: "center",
+    borderRadius: 10,
+    marginTop: 15,
+  },
+});
