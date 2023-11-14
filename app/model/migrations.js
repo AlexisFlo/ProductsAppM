2ad3f5eafdd57ba68585a6a5388d49e793970bc9
Alexito <forthingsLL@hotmail.com>
2023-11-14 12:56:41 -0600

    migration file

--- app/model/migrations.js
+++ app/model/migrations.js
@@ -1,20 +1,7 @@
-import { createTable, schemaMigrations } from "@nozbe/watermelondb/Schema/migrations";
+import { schemaMigrations } from '@nozbe/watermelondb/Schema/migrations'
 
 export default schemaMigrations({
   migrations: [
-    {
-      // ⚠️ set this to a number one larger than the current schema version
-      toVersion: 2,
-      steps: [
-        createTable({
-          name: "comments",
-          columns: [
-            { name: "post_id", type: "string", isIndexed: true },
-            { name: "body", type: "String" },
-          ],
-        }),
-      ],
-    },
     // We'll add migration definitions here later
   ],
-});
+})
