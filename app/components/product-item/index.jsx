3ff2aee726aac9abe32302d2f7f69537bf462892
Flowers <floresfernandezalexis05@gmail.com>
2023-11-15 11:25:29 -0600

    Add opacity animation to ProductItem component

--- app/components/product-item/index.jsx
+++ app/components/product-item/index.jsx
@@ -1,10 +1,15 @@
 import { memo } from "react";
-import { View } from "react-native";
+import Animated, { useAnimatedStyle } from "react-native-reanimated";
 
 import { styles } from "./styles";
 
 const ProductItem = memo(({ item, index, viewableItems }) => {
-  return <View style={styles.item} />;
+  const style = useAnimatedStyle(() => {
+    return {
+      opacity: 1,
+    };
+  });
+  return <Animated.View style={[styles.item, style]} />;
 });
 
 export default ProductItem;
