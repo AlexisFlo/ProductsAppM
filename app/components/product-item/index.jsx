3a5e181e06c489b5e7cc8d6811a852d4c755fb7c
Flowers <floresfernandezalexis05@gmail.com>
2023-11-15 12:38:13 -0600

    Fix useAnimatedStyle in ProductItem component

--- app/components/product-item/index.jsx
+++ app/components/product-item/index.jsx
@@ -5,12 +5,10 @@ import { styles } from "./styles";
 
 const ProductItem = memo(({ item, index, viewableItems }) => {
   const style = useAnimatedStyle(() => {
-    const isVisible = viewableItems.value.filter((item) => item.isViewable);
-    console.warn(isVisible);
     return {
       opacity: 1,
     };
-  }, []);
+  });
   return <Animated.View style={[styles.item, style]} />;
 });
 
