5f30aa77624e041b9e4f41f5a53fa9de848055f1
Flowers <floresfernandezalexis05@gmail.com>
2023-11-17 14:44:25 -0600

    Add input fields and save button to Categories component

--- app/shop/categories.js
+++ app/shop/categories.js
@@ -1,4 +1,4 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, TextInput, StyleSheet, TouchableOpacity, Text } from "react-native";
 
 const styles = StyleSheet.create({
   container: {
@@ -7,12 +7,30 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
   },
+  button: {
+    padding: 20,
+    backgroundColor: "#3B5249",
+    borderRadius: 15,
+    margin: 20,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  buttonText: {
+    fontSize: 13,
+    fontWeight: "bold",
+    color: "#fff",
+  },
 });
 
 export default function Categories() {
   return (
     <View style={styles.container}>
-      <Text>Categories</Text>
+      <TextInput placeholder="Producto" />
+      <TextInput placeholder="Cantidad" keyboardType="numeric" />
+      <TextInput placeholder="$ Precio" keyboardType="numeric" />
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>Guardar</Text>
+      </TouchableOpacity>
     </View>
   );
 }
