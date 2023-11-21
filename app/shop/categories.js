efd757781436df3a282030bbe328c414b51dda4e
Flowers <floresfernandezalexis05@gmail.com>
2023-11-21 12:09:37 -0600

    Add Picker component and useState hook to Categories screen

--- app/shop/categories.js
+++ app/shop/categories.js
@@ -1,3 +1,5 @@
+import { Picker } from "@react-native-picker/picker";
+import { useState } from "react";
 import { View, TextInput, StyleSheet, TouchableOpacity, Text } from "react-native";
 
 const styles = StyleSheet.create({
@@ -31,11 +33,20 @@ const styles = StyleSheet.create({
 });
 
 export default function Categories() {
+  const [selectedValue, setSelectedValue] = useState();
   return (
     <View style={styles.container}>
       <TextInput placeholder="Producto" style={styles.input} />
       <TextInput placeholder="Cantidad" style={styles.input} keyboardType="numeric" />
       <TextInput placeholder="$ Precio" style={styles.input} keyboardType="numeric" />
+      <Text>Categoría</Text>
+      <Picker
+        selectedValue={selectedValue}
+        style={{ height: 50, width: 150 }}
+        onValueChange={(itemValue, itemIndex) => setSelectedValue(itemValue)}>
+        <Picker.Item label="Vegetales" value="Veg" />
+        <Picker.Item label="Frutas" value="Fru" />
+      </Picker>
       <TouchableOpacity style={styles.button}>
         <Text style={styles.buttonText}>Guardar</Text>
       </TouchableOpacity>
