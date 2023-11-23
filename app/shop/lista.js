bac721d3c93b83f78d0b2fadcd604ca4a02266cb
Flowers <floresfernandezalexis05@gmail.com>
2023-11-23 11:28:27 -0600

    Add FlashList component and update dependencies

--- app/shop/lista.js
+++ app/shop/lista.js
@@ -1,45 +1,24 @@
-// import { Picker } from "@react-native-picker/picker";
-// import { useState } from "react";
-import { View, TextInput, StyleSheet, TouchableOpacity, Text } from "react-native";
+import { FlashList } from "@shopify/flash-list";
+import React from "react";
+import { Text } from "react-native";
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    justifyContent: "center",
-    alignItems: "center",
+const DATA = [
+  {
+    title: "Feature Products",
   },
-  input: {
-    borderColor: "#556f75",
-    width: "90%",
-    borderWidth: 1,
-    borderRadius: 10,
-    padding: 10,
-    margin: 15,
+  {
+    title: "Second item",
   },
-  button: {
-    padding: 20,
-    backgroundColor: "#3B5249",
-    borderRadius: 15,
-    margin: 20,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  buttonText: {
-    fontSize: 13,
-    fontWeight: "bold",
-    color: "#fff",
-  },
-});
-// "productList"
-export default function List() {
-  // const [selectedValue, setSelectedValue] = useState();
+];
+
+const List = () => {
   return (
-    <View style={styles.container}>
-      <Text>Lista de compra</Text>
-      {/* 
-        En esta pantalla se debe mostrar la lista de productos que el usuario ha agregado a su lista de compra, y se tendrá la opción de que agregue datos a esta.
-      */}
-    </View>
+    <FlashList
+      data={DATA}
+      renderItem={({ item }) => <Text>{item.title}</Text>}
+      estimatedItemSize={100}
+    />
   );
-}
+};
+
+export default List;
