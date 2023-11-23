37b31ff1b4200b2c0b50287487dd7e37a7e39778
Flowers <floresfernandezalexis05@gmail.com>
2023-11-23 13:24:12 -0600

    Refactor list component to use dynamic data and add removeItem functionality

--- app/shop/lista.js
+++ app/shop/lista.js
@@ -1,22 +1,47 @@
 import { FlashList } from "@shopify/flash-list";
-import React from "react";
-import { Text } from "react-native";
-
-const DATA = [
-  {
-    title: "Feature Products",
-  },
-  {
-    title: "Second item",
-  },
-];
+import React, { useRef, useState } from "react";
+import { LayoutAnimation, Pressable, Text } from "react-native";
+import { View } from "react-native-web";
 
 const List = () => {
+  const [data, setData] = useState([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+
+  const list = useRef(null);
+
+  const removeItem = (item) => {
+    setData(
+      data.filter((dataItem) => {
+        return dataItem !== item;
+      })
+    );
+    list.current?.prepareForLayoutAnimationRender();
+
+    LayoutAnimation.configureNext(LayoutAnimation.Presets.easeInEaseOut);
+  };
+
+  const renderItem = ({ item }) => {
+    return (
+      <Pressable
+        onPress={() => {
+          // removeItem(item);
+          console.warn("pressed");
+        }}>
+        <View>
+          <Text>Cell Id:</Text>
+        </View>
+      </Pressable>
+    );
+  };
+
   return (
     <FlashList
-      data={DATA}
-      renderItem={({ item }) => <Text>{item.title}</Text>}
+      ref={list}
+      keyExtractor={(item) => {
+        return item.toString();
+      }}
+      renderItem={renderItem}
       estimatedItemSize={100}
+      data={data}
     />
   );
 };
